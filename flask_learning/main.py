c6ecc8e683486aba2ee54f191a774dc2a6eb60ef
yanglu <yludy@139.com>
2020-04-17 18:43:02 +0800

    flask-learning

--- flask_learning/main.py
+++ flask_learning/main.py
@@ -1,19 +1,55 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,EqualTo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'zhoubing'
 
+
+
+
+
+#配置数据库地址
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://127.0.0.1:3306/mytest'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('用户名：',validators=[DataRequired()])
+    password = PasswordField('密码：',validators=[DataRequired()])
+    password2 = PasswordField('确认密码：',validators=[DataRequired(),EqualTo('password','密码填入不一致')])
+    submit = SubmitField('提交')
+
+@app.route('/form',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if request.method == 'POST':
+        username = request.form.get('username') 
+        password = request.form.get('password') 
+        password2 = request.form.get('password2') 
+        if login_form.validate_on_submit():
+            print(username,password)
+            return 'success'
+        else:
+            flash('参数有误')
+    return render_template('index.html',form=login_form)
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
     # return 'hello world!'
-    url_str = 'www.baidu.com'
-    my_list = [1,2,3,4,5]
-    my_dict = {
-        'name':'zhangsan',
-        'address':'乐山市'
-            }
+    # url_str = 'www.baidu.com'
+    # my_list = [1,2,3,4,5]
+    # my_dict = {
+    #     'name':'zhangsan',
+    #     'address':'乐山市'
+    #         }
     return render_template('index.html',url_str=url_str,my_list=my_list,my_dict=my_dict)
 
 
