123e95cf18e75341eab070d7f3c5daf1efa5977e
yanglu <yludy@139.com>
2020-04-24 20:02:31 +0800

    demo

--- vue_learn/vue_first/demo/src/main.js
+++ vue_learn/vue_first/demo/src/main.js
@@ -4,10 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-
+import './assets/css/global.css'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import axios from 'axios'
 Vue.config.productionTip = false
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:5000/'
+Vue.prototype.$http = axios
+
+Vue.prototype.HOST = '/api'
 
 Vue.use(ElementUI)
 /* eslint-disable no-new */
--- vue_learn/vue_first/demo/src/router/index.js
+++ vue_learn/vue_first/demo/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from './component/Login.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/login', component: Login }
+    {path:'/',redirect:'/login'},
+    { path: '/login',name:'Login', component: Login },
+    { path: '/home',name:'Home', component: Home }
   ]
 })
